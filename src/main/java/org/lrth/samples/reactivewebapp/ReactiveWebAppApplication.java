c70d45be4123da7b841ff4bdec88a3726b2f469f
LennarthAnaya <lanaya@corelogic.com>
2019-07-12 12:59:54 -0500

    Spring scafolding

---
+++ src/main/java/org/lrth/samples/reactivewebapp/ReactiveWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.lrth.samples.reactivewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebAppApplication.class, args);
+	}
+
+}
